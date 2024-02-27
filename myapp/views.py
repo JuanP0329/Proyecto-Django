d7394df434636afc735e7e15a881a007e6340e11
JuanP0329 <jpbenjumeago@cesde.net>
2024-02-27 10:36:08 -0500

    Se crea una ruta para recibir parametros en el path

--- myapp/views.py
+++ myapp/views.py
@@ -2,8 +2,11 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hello World<h1>")
+def index(request):
+    return HttpResponse("<h1>Index page<h1>")
+
+def hello(request, username):
+    return HttpResponse("<h1>Hello %s</h1>"%username)
 
 def about(request):
     return HttpResponse("<h1>About<h1>")
